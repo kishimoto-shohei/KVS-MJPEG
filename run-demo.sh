5841209c932ca5393adb1f3a821ee8be2f34b21d
Shohei KISHIMOTO <36260153+kishimoto-shohei@users.noreply.github.com>
2018-08-17 12:55:04 +0900

    Add files via upload

--- run-demo.sh
+++ run-demo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$#" != 6 ]; then
- echo " Usage: ./run-java-demoapp.sh access_key secret_key "
+if [ "$#" != 7 ]; then
+ echo " Usage: ./run-demo.sh access_key secret_key video_src user pass stream_name region "
  exit
 fi
 ACCESS_KEY=$1
@@ -10,8 +10,9 @@ VIDEO_SRC=$3
 USER=$4
 PASS=$5
 STREAM_NAME=$6
+REGION=$7
 
-mvn package
+mvn install
 # Create a temporary filename in /tmp directory
 jar_files=$(mktemp)
 # Create classpath string of dependencies from the local repository to a file
@@ -19,5 +20,5 @@ mvn -Dmdep.outputFile=$jar_files dependency:build-classpath
 export LD_LIBRARY_PATH=/opt/amazon-kinesis-video-streams-producer-sdk-cpp/kinesis-video-native-build/downloads/local/lib:$LD_LIBRARY_PATH
 classpath_values=$(cat $jar_files)
 # Start the demo app
-java -classpath target/kvsdemo-1.0-SNAPSHOT.jar:$classpath_values -Daws.accessKeyId=${ACCESS_KEY} -Daws.secretKey=${SECRET_KEY} -Djava.library.path=/opt/amazon-kinesis-video-streams-producer-sdk-cpp/kinesis-video-native-build brainstech.kvsdemo.OpenCVDemo $3 $4 $5 $6
+java -classpath target/kvsdemo-1.0-SNAPSHOT.jar:$classpath_values -Daws.accessKeyId=${ACCESS_KEY} -Daws.secretKey=${SECRET_KEY} -Djava.library.path=/opt/amazon-kinesis-video-streams-producer-sdk-cpp/kinesis-video-native-build brainstech.kvsdemo.OpenCVDemo ${VIDEO_SRC} ${USER} ${PASS} ${STREAM_NAME} ${REGION}
 
