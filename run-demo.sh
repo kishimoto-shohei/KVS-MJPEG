cfe4ee390220e8de43d224a2a56b413b01f073bf
Shohei KISHIMOTO <36260153+kishimoto-shohei@users.noreply.github.com>
2018-10-31 14:34:01 +0900

    Update run-demo.sh

--- run-demo.sh
+++ run-demo.sh
@@ -12,13 +12,12 @@ PASS=$5
 STREAM_NAME=$6
 REGION=$7
 
-mvn install
 # Create a temporary filename in /tmp directory
-jar_files=$(mktemp)
+#jar_files=$(mktemp)
 # Create classpath string of dependencies from the local repository to a file
-mvn -Dmdep.outputFile=$jar_files dependency:build-classpath
-export LD_LIBRARY_PATH=/opt/amazon-kinesis-video-streams-producer-sdk-cpp/kinesis-video-native-build/downloads/local/lib:$LD_LIBRARY_PATH
-classpath_values=$(cat $jar_files)
+#mvn -Dmdep.outputFile=$jar_files dependency:build-classpath
+#export LD_LIBRARY_PATH=/opt/amazon-kinesis-video-streams-producer-sdk-cpp/kinesis-video-native-build/downloads/local/lib:$LD_LIBRARY_PATH
+#classpath_values=$(cat $jar_files)
 # Start the demo app
 java -classpath target/kvsdemo-1.0-SNAPSHOT.jar:$classpath_values -Daws.accessKeyId=${ACCESS_KEY} -Daws.secretKey=${SECRET_KEY} -Djava.library.path=/opt/amazon-kinesis-video-streams-producer-sdk-cpp/kinesis-video-native-build brainstech.kvsdemo.OpenCVDemo ${VIDEO_SRC} ${USER} ${PASS} ${STREAM_NAME} ${REGION}
 
